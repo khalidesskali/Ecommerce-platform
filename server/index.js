93522dca79f2f1ca0d2a1443d3a039c43b588e50
khalidesskali <esskalikhalid7@gmail.com>
2025-06-07 13:24:57 +0100

    project setup

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express backend");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
